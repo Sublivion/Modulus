c70967a239e16077b4782fc837222672eadc83e6
Calvin649 <35707351+Calvin649@users.noreply.github.com>
2018-07-13 18:38:52 -0400

    Add files via upload

---
+++ commands/leave.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+module.exports.run = async(bot, message, args) => {
+  if(!args[0]){
+    return message.channel.send({embed:{
+      color: 15158332,
+      fields: [{
+        name: "Error",
+        value: "Please specify a role name to join"
+      },
+    ],
+    }});
+  }
+
+  let roles = JSON.parse(fs.readFileSync("./roles.json", "utf8"));
+
+  if(!roles[args[0]]){
+    return message.channel.send({embed:{
+      color: 15158332,
+      fields: [{
+        name: "Error",
+        value: "This role does not exist"
+      },
+    ],
+    }});
+  };
+
+  message.member.removeRole(roles[args[0]].roleName);
+
+  return message.channel.send({embed:{
+    color: 15844367,
+    fields: [{
+      name: "Success",
+      value: `Successfully left **${args[0]}**`
+    }],
+  }});
+
+}
+
+module.exports.config = {
+  name: "leave"
+}
