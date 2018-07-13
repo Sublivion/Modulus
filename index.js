c0d0e44d1d9ab8c5a145cb3aa9e55ad1bd11635d
Calvin649 <35707351+Calvin649@users.noreply.github.com>
2018-07-13 18:32:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const webMod = require("./webmod.js");
 const Discord = require("discord.js");
 const fs = require("fs");
 const http = require("http");
-const express = require("express");
 
 const bot = new Discord.Client({disableEveryone: false});
-const app = express();
 bot.commands = new Discord.Collection()
 
 http.createServer(function(req, res) {
@@ -52,7 +50,14 @@ bot.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
 
-  let prefix = config.prefix;
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf-8"));
+  if(!prefixes[message.guild.id]){
+    prefixes[message.guild.id] = {
+      prefixes: config.prefix
+    };
+  };
+
+  let prefix = prefixes[message.guild.id].prefixes
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
