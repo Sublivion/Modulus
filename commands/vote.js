72bee8a72c32962ef893a89cfe5cfa134cdf2030
Calvin649 <35707351+Calvin649@users.noreply.github.com>
2018-07-15 19:38:50 -0400

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -1,4 +1,6 @@
 const Discord = require("discord.js");
+const agree = "✅";
+const disagree = "❌";
 
 module.exports.run = async(bot, message, args) => {
 
@@ -27,17 +29,27 @@ module.exports.run = async(bot, message, args) => {
   };
 
   let embed = new Discord.RichEmbed()
-    .setColor("f1c40f")
-    .setFooter("React to vote")
+    .setColor("#f1c40f")
+    .setFooter("React to vote, results will be posted in 15 seconds")
     .setDescription(input)
     .setTitle(`Vote initiated by ${message.author.username}`);
 
   let msg = await message.channel.send(embed);
 
-  await msg.react("✅");
-  await msg.react("❌");
+  await msg.react(agree);
+  await msg.react(disagree);
 
   message.delete({timeout: 1000});
+
+  const reactions = await msg.awaitReactions(reaction => reaction.emoji.name === agree || reaction.emoji.name === disagree, {time: 15000});
+  return message.channel.send({embed:{
+    color: 15844367,
+    fields: [{
+      name: "Voting Results",
+      value: `Voting Complete! \n\n${agree}: ${reactions.get(agree).count-1}\n${disagree}: ${reactions.get(disagree).count-1}`
+    },
+  ],
+  }});
 
 }
 
