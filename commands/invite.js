693be897ae5d4e26b96505dddf093c0bc2853aaf
Calvin649 <35707351+Calvin649@users.noreply.github.com>
2018-07-25 23:00:01 -0400

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -5,10 +5,10 @@ const Discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
   return message.channel.send({embed:{
     color: 15844367,
-    url: "https://discordapp.com/oauth2/authorize?client_id=355458309333188609&permissions=8&scope=bot",
+    url: "https://discordapp.com/oauth2/authorize?client_id=468134631221362698&permissions=8&scope=bot",
     fields: [{
       name: "Bot Invite Link",
-      value: "To invite Modulus to your server, click this [link!](https://discordapp.com/oauth2/authorize?client_id=355458309333188609&permissions=8&scope=bot)"
+      value: "To invite Modulus to your server, click this [link!](https://discordapp.com/oauth2/authorize?client_id=468134631221362698&permissions=8&scope=bot)"
     },
   ],
   }});
